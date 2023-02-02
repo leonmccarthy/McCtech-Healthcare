87e746e5d07b047378c351a505edccd65335f197
leonmccarthy <mccarthymu@ueab.ac.ke>
2023-02-02 12:45:40 +0300

    Designed doctor details

---
+++ app/src/main/java/com/example/healthcare/DoctorDetailsActivity.java
@@ -0,0 +1,34 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class  DoctorDetailsActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_details);
+
+        tv = findViewById(R.id.textViewDDTitle);
+        Intent it = getIntent();
+        String title = it.getStringExtra("title");
+        tv.setText(title);
+
+        back = findViewById(R.id.buttonDDBack);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DoctorDetailsActivity.this, FindDoctorActivity.class));
+            }
+        });
+    }
+}
