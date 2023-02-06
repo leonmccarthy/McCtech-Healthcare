8e35e4dc55fca58ea935174dae0404b976472aab
leonmccarthy <mccarthymu@ueab.ac.ke>
2023-02-06 12:54:59 +0300

    Date and time functionality

--- app/src/main/java/com/example/healthcare/BookAppointmentActivity.java
+++ app/src/main/java/com/example/healthcare/BookAppointmentActivity.java
@@ -2,15 +2,30 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class BookAppointmentActivity extends AppCompatActivity {
 
     EditText ed1, ed2, ed3, ed4;
     TextView tv;
+
+    Button dateButton, timeButton, bookAppointmentButton, backButton;
+
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +35,11 @@ public class BookAppointmentActivity extends AppCompatActivity {
         ed2 = findViewById(R.id.editTextBAAddress);
         ed3 = findViewById(R.id.editTextBAContactNo);
         ed4 = findViewById(R.id.editTextBAFee);
+        timeButton = findViewById(R.id.buttonBATime);
+        dateButton = findViewById(R.id.buttonBADate);
+        bookAppointmentButton = findViewById(R.id.buttonBABookAppintment);
+        backButton = findViewById(R.id.buttonBABack);
+
 
         ed1.setKeyListener(null);
         ed2.setKeyListener(null);
@@ -38,5 +58,64 @@ public class BookAppointmentActivity extends AppCompatActivity {
         ed2.setText(address);
         ed3.setText(contact);
         ed4.setText(fees);
+
+//        datepicker
+        initDatePicker();
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
+
+//        timepicker
+        initTimePicker();
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePickerDialog.show();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(BookAppointmentActivity.this, FindDoctorActivity.class));
+            }
+        });
+    }
+
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                i1=i1+1;
+                dateButton.setText(i2+"/"+i1+"/"+i);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(cal.getTimeInMillis()+86400000);
+    }
+
+    private void initTimePicker(){
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                timeButton.setText(i+":"+i1);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int hrs = cal.get(Calendar.HOUR);
+        int min = cal.get(Calendar.MINUTE);
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+        timePickerDialog = new TimePickerDialog(this, style, timeSetListener, hrs, min, true);
     }
 }
