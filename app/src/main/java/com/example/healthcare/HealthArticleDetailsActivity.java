a36cb07bc04f7ce5c7dcaff651c8c1cc53082794
leonmccarthy <mccarthymu@ueab.ac.ke>
2023-02-08 15:19:00 +0300

    design & functionality for health articles details page

--- app/src/main/java/com/example/healthcare/HealthArticleDetailsActivity.java
+++ app/src/main/java/com/example/healthcare/HealthArticleDetailsActivity.java
@@ -2,13 +2,41 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HealthArticleDetailsActivity extends AppCompatActivity {
 
+    TextView tvTitle;
+    ImageView img;
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_article_details);
+
+        tvTitle = findViewById(R.id.textViewHADTitle);
+        img = findViewById(R.id.imageViewHAD);
+        backButton = findViewById(R.id.buttonHADBack);
+
+        Intent it = getIntent();
+        tvTitle.setText(it.getStringExtra("text1"));
+
+        Bundle bundle =  getIntent().getExtras();
+        if(bundle !=null){
+            int resId = bundle.getInt("text2");
+            img.setImageResource(resId);
+        }
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HealthArticleDetailsActivity.this, HealthArticlesActivity.class));
+            }
+        });
     }
 }
