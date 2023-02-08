ad1bd24dac3ea03b8f4d84b6072cc62790f84f89
leonmccarthy <mccarthymu@ueab.ac.ke>
2023-02-08 14:11:02 +0300

    functionality for health articles page

--- app/src/main/java/com/example/healthcare/HealthArticlesActivity.java
+++ app/src/main/java/com/example/healthcare/HealthArticlesActivity.java
@@ -2,7 +2,10 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -31,12 +34,48 @@ public class HealthArticlesActivity extends AppCompatActivity {
     HashMap<String, String> item;
     ArrayList list;
     SimpleAdapter sa;
-    ListView lvOrders;
+    ListView lvArticles;
     Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_articles);
+
+        lvArticles = findViewById(R.id.listViewHAmultiline);
+        backButton = findViewById(R.id.buttonHABack);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HealthArticlesActivity.this, HomeActivity.class));
+            }
+        });
+
+        list = new ArrayList();
+        for (int i=0;i<health_details.length;i++){
+            item = new HashMap<String, String>();
+            item.put("line1", health_details[i][0]);
+            item.put("line2", health_details[i][1]);
+            item.put("line3", health_details[i][2]);
+            item.put("line4", health_details[i][3]);
+            item.put("line5", health_details[i][4]);
+            list.add(item);
+        }
+        sa = new SimpleAdapter(this, list, R.layout.multi_line,
+                new String[]{"line1", "line2", "line3", "line4", "line5"},
+                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e});
+
+        lvArticles.setAdapter(sa);
+
+        lvArticles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(HealthArticlesActivity.this, HealthArticleDetailsActivity.class);
+                it.putExtra("text1", health_details[i][0]);
+                it.putExtra("text2", images[i]);
+                startActivity(it);
+            }
+        });
     }
 }
